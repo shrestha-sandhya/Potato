202e19ff57f2e2b31b2d06d7a4b478dbb9f7d3ac
shrestha-sandhya <140813154+shrestha-sandhya@users.noreply.github.com>
2023-08-08 13:55:51 +0545

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,5 +151,5 @@ inputFile.addEventListener("change", uploadImage);
 function uploadImage(){
     let imgLink = URL.createObjectURL(inputFile.files[0]);
     imageView.style.backgroundImage = 'url(${imgLink})';
-    imageView.textContent = ;
+    imageView.textContent = "";
 }
